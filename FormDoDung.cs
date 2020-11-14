0dc1970e8d897713a838e8d162c7f98604fcf97d
0962769565 <74644355+0962769565@users.noreply.github.com>
2020-11-14 15:43:19 +0700

    Them do dung

---
+++ FormDoDung.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class FormDoDung : Form
+    {
+        public FormDoDung()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDoDung_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+        private DataTable table;
+        private Connection connector = new Connection();
+
+        private void reset()
+        {
+            listView1.Items.Clear();
+            table = new DataTable();
+            table = connector.LoadData("3");
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void reset2()
+        {
+            listView1.Items.Clear();
+            table = connector.FindObject("3", textSearch.Text.Trim());
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                textID.Text = table.Rows[index][0].ToString();
+                textName.Text = table.Rows[index][1].ToString();
+                textPrice.Text = table.Rows[index][2].ToString();
+            }
+        }
+
+        private void radView_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private bool check(int k)
+        {
+            if (k == 1)
+            {
+                if (textID.Text.Trim() == "")
+                {
+                    MessageBox.Show("ID's values must not be null", "O___O", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    textID.Focus();
+                    return false;
+                }
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    if (textID.Text.Trim() == table.Rows[i][0].ToString().Trim())
+                    {
+                        MessageBox.Show("ID's values has been taken, please input new ID", "O___O", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        textID.Focus();
+                        return false;
+                    }
+                }
+            }
+            if (textName.Text.Trim() == "")
+            {
+                MessageBox.Show("State's values must not be null", "O___O", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textName.Focus();
+                return false;
+            }
+            if (textPrice.Text.Trim() == "")
+            {
+                MessageBox.Show("Price's values must not be null", "O___O", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPrice.Focus();
+                return false;
+            }
+            int b;
+            if (int.TryParse(textPrice.Text, out b) == false)
+            {
+                MessageBox.Show("This Price per day value is not valid", "O___O", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPrice.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void butFree_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
