331b95601105329a75b0c0e545243cf749e6ba7d
ThanhHa0806 <riverandsnow300418@gmail.com>
2020-11-16 21:28:14 +0700

    Update module Quản lý phòng
    Load dữ liệu lên form

---
+++ QuanLyKhachSan/FormPhong.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class FormPhong : Form
+    {
+        public FormPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPhong_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private DataTable table;
+        private Connection connector = new Connection();
+
+        private void reset()
+        {
+            listView1.Items.Clear();
+            table = new DataTable();
+            table = connector.LoadData("2");
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void reset2()
+        {
+            listView1.Items.Clear();
+            table = connector.FindObject("2", textSearch.Text.Trim());
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                textID.Text = table.Rows[index][0].ToString();
+                textState.Text = table.Rows[index][1].ToString();
+                textCost.Text = table.Rows[index][2].ToString();
+                textNote.Text = table.Rows[index][3].ToString();
+            }
+        }
+
+        private void radView_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rad = (RadioButton)(sender);
+            if (rad.Checked == true)
+            {
+                butFree.Text = rad.Text;
+                if (rad.Text == "View")
+                {
+                    butFree.Enabled = false;
+                    textCost.ReadOnly = textState.ReadOnly = textID.ReadOnly = textNote.ReadOnly = true;
+                }
+                else
+                {
+                    butFree.Enabled = true;
+                    textID.ReadOnly = true;
+                    textCost.ReadOnly = textState.ReadOnly = textNote.ReadOnly = false;
+                    if (rad.Text == "Add")
+                    {
+                        textID.ReadOnly = false;
+                        textCost.Text = textState.Text = textID.Text = textNote.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private bool check(int k)
+        {
+
+        }
+
+        private void butFree_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
