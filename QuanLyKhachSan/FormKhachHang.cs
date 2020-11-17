94ae6e97d23edeeed942654b26ab892a5f07f8fe
0962769565 <74644355+0962769565@users.noreply.github.com>
2020-11-17 19:53:56 +0700

    Them Khach Hang

---
+++ QuanLyKhachSan/FormKhachHang.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class FormKhachHang : Form
+    {
+        public FormKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void FormKhachHang_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private DataTable table;
+        private Connection connector = new Connection();
+
+        private void reset()
+        {
+            listView1.Items.Clear();
+            table = new DataTable();
+            table = connector.LoadData("1");
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void reset2()
+        {
+            listView1.Items.Clear();
+            table = connector.FindObject("1", textSearch.Text.Trim());
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                textID.Text = table.Rows[index][0].ToString();
+                textName.Text = table.Rows[index][1].ToString();
+                textCMND.Text = table.Rows[index][2].ToString();
+                textPhone.Text = table.Rows[index][3].ToString();
+            }
+        }
+
+        private void radView_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private bool check(int k)
+        {
+            
+        }
+
+        private void butFree_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
